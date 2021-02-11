ca6855b62f2b07cc0d82162cde233fed439419a9
Sebastian Porling <sebastian.porling@gmail.com>
2021-02-11 10:49:43 +0100

    feat: add bash script for running multiple simulations

---
+++ multiple_simulations.sh
@@ -0,0 +1,9 @@
+echo "Running 10 simulations with 3 doors"
+java -jar MontyHallSimulation 10 3
+echo "\nRunning 100 simulations with 3 doors"
+java -jar MontyHallSimulation 100 3
+echo "\nRunning 10000 simulations with 3 doors"
+java -jar MontyHallSimulation 10000 3
+echo "\nRunning 1000000 simulations with 3 doors"
+java -jar MontyHallSimulation 1000000 3
+
